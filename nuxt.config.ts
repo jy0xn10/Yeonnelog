d7f33fcaafa2b2e3dfc42d602c625e78ce0f5f76
jy0xn <jy0xn10@gmail.com>
2026-08-03 22:17:02 +0900

    feat: fix baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   app: {
-    baseURL: '/yeonnelog/',
+    baseURL: '/Yeonnelog/',
   },
   css: [
     'destyle.css',
